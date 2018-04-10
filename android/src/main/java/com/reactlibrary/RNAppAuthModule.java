e268e34b772fa0f1472e4a5954c98abf8de7a6bc
Xiaoyu Yin <yin530@gmail.com>
2018-04-10 15:31:15 -0700

    test

--- android/src/main/java/com/reactlibrary/RNAppAuthModule.java
+++ android/src/main/java/com/reactlibrary/RNAppAuthModule.java
@@ -206,7 +206,7 @@ public class RNAppAuthModule extends ReactContextBaseJavaModule implements Activ
      * Called when the OAuth browser activity completes
      */
     @Override
-    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+    public void onActivityResult(Activity activity, int requestCode, int resultCode, Intent data) {
         if (requestCode == 0) {
             AuthorizationResponse response = AuthorizationResponse.fromIntent(data);
             AuthorizationException exception = AuthorizationException.fromIntent(data);
